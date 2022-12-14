7871c9cdfd4e64f4b03ed0f3af98c65f5f7324e1
Jarvis <Jarvis.C.Huang@newegg.com>
2022-12-14 22:54:07 +0800

    add style.css setting

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import _ from 'lodash';
+import './style.css'
 
 function component() {
     const element = document.createElement('div');
 
     // Lodash, now imported by this scriptokok
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
+    element.classList.add('hello');
     return element;
 }
 
